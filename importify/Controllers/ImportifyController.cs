b559d44c443eec6fd8b166d21375f98ecb0cfa24
artem_gck <g.artema31@gmail.com>
2022-03-14 14:25:36 +0300

    Add structure of project.

---
+++ importify/Controllers/ImportifyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Importify.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImportifyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return null;
+        }
+    }
+}
--- importify/Program.cs
+++ importify/Program.cs
@@ -3,24 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddDbContext<ImportifyContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddMvc();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseStatusCodePages();
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
