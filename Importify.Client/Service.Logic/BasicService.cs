13903fba0e973121d7ab259673c68e0535626713
artem_gck <g.artema31@gmail.com>
2022-04-17 23:00:46 +0300

    Add support of refresh token.

--- Importify.Client/Service.Logic/BasicService.cs
+++ Importify.Client/Service.Logic/BasicService.cs
@@ -2,6 +2,7 @@
 using Importify.Client.Model;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 namespace Importify.Client.Service.Logic
 {
@@ -25,15 +26,61 @@ namespace Importify.Client.Service.Logic
 
         public async Task<List<Country>> GetCountryes()
         {
+            string responseString;
+            List<Country> countries;
+
             var cookieContent = await _storageService.GetItemAsync<string>("access_token");
+            
+            if (cookieContent is null)
+                return null;
+            
             _httpClient.DefaultRequestHeaders.Add("access_token", cookieContent);
 
             var response = await _httpClient.GetAsync("basic/countries");
-            var responseString = await response.Content.ReadAsStringAsync();
 
-            var countries = JsonConvert.DeserializeObject<List<Country>>(responseString);
+            if ((int)response.StatusCode == 401)
+            {
+                if (await SendRefreshToken(cookieContent) == -1)
+                    return null;
+
+                response = await _httpClient.GetAsync("basic/countries");
+
+                responseString = await response.Content.ReadAsStringAsync();
+                countries = JsonConvert.DeserializeObject<List<Country>>(responseString);
+
+                return countries;
+            }
+
+            responseString = await response.Content.ReadAsStringAsync();
+            countries = JsonConvert.DeserializeObject<List<Country>>(responseString);
 
             return countries;
+        }
+
+        private async Task<int> SendRefreshToken(string cookieContent)
+        {
+            var tok = new Tokens()
+            {
+                AccessToken = cookieContent,
+                RefreshToken = await _storageService.GetItemAsync<string>("refresh_token"),
+            };
+
+            var resp = await _httpClient.PostAsJsonAsync("token/refresh", tok);
+
+            if ((int)resp.StatusCode == 404)
+                return -1;
+
+            var respString = await resp.Content.ReadAsStringAsync();
+            var tokens = JsonConvert.DeserializeObject<Tokens>(respString);
+
+            await _storageService.SetItemAsStringAsync("access_token", tokens.AccessToken);
+            await _storageService.SetItemAsStringAsync("refresh_token", tokens.RefreshToken);
+
+            cookieContent = await _storageService.GetItemAsync<string>("access_token");
+            _httpClient.DefaultRequestHeaders.Remove("access_token");
+            _httpClient.DefaultRequestHeaders.Add("access_token", cookieContent);
+
+            return 0;
         }
     }
 }
