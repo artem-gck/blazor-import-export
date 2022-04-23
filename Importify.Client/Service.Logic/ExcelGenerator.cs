51491fa8463af8b8fae6dc3df8621fe165056a3a
artem_gck <g.artema31@gmail.com>
2022-04-23 13:57:23 +0300

    Add excel download.

--- Importify.Client/Service.Logic/ExcelGenerator.cs
+++ Importify.Client/Service.Logic/ExcelGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using Importify.Client.Model;
 using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.Drawing;
 
 namespace Importify.Client.Service.Logic
 {
@@ -28,7 +30,77 @@ namespace Importify.Client.Service.Logic
                 row++;
             }
 
+            SetStyle(sheet, 4, row);
+
             return await package.GetAsByteArrayAsync();
+        }
+
+        public async Task<byte[]> GenerateCategoryExportCategoryTable(List<CountryConstituent> data)
+        {
+            var package = new ExcelPackage();
+
+            var sheet = package.Workbook.Worksheets.Add("Default");
+
+            sheet.Cells[1, 1].Value = "Год";
+            sheet.Cells[1, 2].Value = "Категория";
+            sheet.Cells[1, 3].Value = "Экспорт";
+
+            var row = 2;
+
+            foreach (var item in data)
+            {
+                sheet.Cells[row, 1].Value = item.Year;
+                sheet.Cells[row, 2].Value = item.Constituent;
+                sheet.Cells[row, 3].Value = item.Value;
+
+                row++;
+            }
+
+            SetStyle(sheet, 3, row);
+
+            return await package.GetAsByteArrayAsync();
+        }
+
+        public async Task<byte[]> GenerateCategoryExportPlot(List<CountryConstituentPlot> data)
+        {
+            var package = new ExcelPackage();
+
+            var sheet = package.Workbook.Worksheets.Add("Default");
+
+            sheet.Cells[1, 1].Value = "Категория";
+            sheet.Cells[1, 2].Value = "Экспорт";
+
+            var row = 2;
+
+            foreach (var item in data)
+            {
+                sheet.Cells[row, 1].Value = item.Constituent;
+                sheet.Cells[row, 2].Value = item.Value;
+
+                row++;
+            }
+
+            SetStyle(sheet, 2, row);
+
+            return await package.GetAsByteArrayAsync();
+        }
+
+        private void SetStyle(ExcelWorksheet sheet, int colomn, int row)
+        {
+            sheet.Cells[1, 1, row - 1, colomn].AutoFitColumns();
+
+            for (var i = 1; i < colomn + 1; i++)
+                sheet.Column(i).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+            for (var x = 1; x < colomn + 1; x++)
+                for (var y = 1; y < row; y++)
+                    sheet.Cells[y, x].Style.Border.BorderAround(ExcelBorderStyle.Thin);
+
+            for (var x = 1; x < colomn + 1; x++)
+            {
+                sheet.Cells[1, x].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                sheet.Cells[1, x].Style.Fill.BackgroundColor.SetColor(Color.LightGreen);
+            }
         }
     }
 }
--- Importify.Client/Service/IExcelGenerator.cs
+++ Importify.Client/Service/IExcelGenerator.cs
@@ -5,5 +5,7 @@ namespace Importify.Client.Service
     public interface IExcelGenerator
     {
         public Task<byte[]> GenerateTotalImportExportTable(List<CountryImportExport> data);
+        public Task<byte[]> GenerateCategoryExportCategoryTable(List<CountryConstituent> data);
+        public Task<byte[]> GenerateCategoryExportPlot(List<CountryConstituentPlot> data);
     }
 }
